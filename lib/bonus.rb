58fe23bdcbb342c3e88b693b386bf9c4a9345fb2
Olivia Madden <madden.livi@gmail.com>
2019-12-23 19:13:05 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,16 +19,17 @@ def bonus
       ]
    }
   }
-end 
+   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+end
 
+ #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
 def bonus_method(bonus)
   epic_tragedy[:montague][:hero][:status]= "dead"
   epic_tragedy[:capulet][:heroine][:status]= "dead"
   puts epic_tragedy
-end
 
   
 
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+ 
   epic_tragedy
 end
