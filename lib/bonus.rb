e0193a32dbe3e12922c1e0036c16eae25181d022
Olivia Madden <madden.livi@gmail.com>
2019-12-23 19:05:21 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,7 +20,7 @@ def bonus
    }
   }
 
-def bonus_method(epic_tragedy)
+def bonus_method
   epic_tragedy[:montague][:hero][:status]= "dead"
   epic_tragedy[:capulet][:heroine][:status]= "dead"
   puts epic_tragedy
