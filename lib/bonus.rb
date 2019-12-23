5cbfae3186435172faae6531ed3b64d461eae9f2
Olivia Madden <madden.livi@gmail.com>
2019-12-23 19:17:31 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,14 +19,10 @@ def bonus
       ]
    }
   }
+  
+   epic_tragedy[:montague][:hero][:status]= "dead"
+  epic_tragedy[:capulet][:heroine][:status]= "dead"
    #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
    epic_tragedy
 end
 
-def bonus_method
-  epic_tragedy = bonus
-  epic_tragedy[:montague][:hero][:status]= "dead"
-  epic_tragedy[:capulet][:heroine][:status]= "dead"
-
-  epic_tragedy
-end
