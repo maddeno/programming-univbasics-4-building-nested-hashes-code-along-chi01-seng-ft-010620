2293d33ae30682ac0ab872beed8fd16284c6dcca
Olivia Madden <madden.livi@gmail.com>
2019-12-23 18:56:57 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -26,13 +26,14 @@ def fourth_challenge
           :name => "Steven",
           :age => "30",
           :attitude => "confused"
-        }
+        },
         
         {
           :name => "Nurse",
           :age => "44",
           :attitude => "worried"
-        }]
+        }
+      ]
    }
 }
 
